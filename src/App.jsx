37bac46cab9ac1f59f91d64c81928b4fb3226e4d
Hannah Lin <ht.techx@gmail.com>
2024-02-24 12:01:16 -0500

    adding lists

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,33 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// Above the App() function, create an empty Array and store it in a variable named todoList
+// Inside the Array, create at least 3 Objects with the properties
+const todoList = [
+  { id: 1, title: "HelloCoding" },
+  { id: 2, title: "TodayIsSaturday" },
+  { id: 3, title: "CompleteAssignment" }
+]
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
+        <h1>Todo List</h1>
+        <ul>
+          {/* Inside the JavaScript expression, map over your todoList array
+              For each Object in the Array, return a list item (<li>) with the following:
+                key attribute with value of id property
+                inner text content with value of title property */}
+          {/* using variable name todo to represent each element/object in the array */}
+          {todoList.map(todo => (
+            <li key={todo.id}>{todo.title}</li>
+          ))}
+        </ul>
+      </div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +48,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
