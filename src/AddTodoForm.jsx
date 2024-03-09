f352f6982954c497e7e4edef9ab650a8427a93d1
Hannah Lin <ht.techx@gmail.com>
2024-03-09 13:14:09 -0500

    refactor list items, handle form submit, and store new item in React State

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,10 +1,23 @@
+// Handle "Add Todo Form" Submit
+
 import React from 'react';
 
-function AddTodoForm() {
+function AddTodoForm(props) {
+    function handleAddTodo (event) {
+        event.preventDefault()
+        // retrieve the value of the title element from the event target and store it in a variable named todoTitle
+        const todoTitle = event.target.elements.title.value;
+        // invoke the onAddTodo callback prop and pass todoTitle as an argument
+        props.onAddTodo(todoTitle)
+        // reset the form so the text input value is cleared
+        event.target.reset();
+    }
+    
     return (
-        <form>
+        // Add onSubmit prop to form element and pass the handleAddTodo function by reference
+        <form onSubmit={handleAddTodo}>
             <label htmlFor="todoTitle">Title</label>
-            <input type="text" id="todoTitle" />
+            <input type="text" id="todoTitle" name="title"/>
             <button type="submit">Add</button>
         </form>
     )
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+// Store "New Todo" in React State
+
+import React, { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,11 +10,18 @@ import AddTodoForm from './AddTodoForm'
 function App() {
   // const [count, setCount] = useState(0)
 
+  // create a new state variable named newTodo with update function named setNewTodo
+  const [newTodo,setNewTodo] = useState('')
+
   return (
     <>
       <div>
         <h1>Todo List</h1>
-        <AddTodoForm />
+        <AddTodoForm onAddTodo={setNewTodo}/>
+        
+        {/* displays the value of newTodo variable */}
+        <p>New Todo: {newTodo}</p>
+        
         <TodoList />
       </div>
     </>
