5d115603ea5a295ebdc8422dac9913bb56327458
Hannah Lin <ht.techx@gmail.com>
2024-05-22 21:32:54 -0400

    async, add loading state, and create conditional loading indicator

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,30 @@ import './App.css'
 import TodoList from './TodoList'
 import AddTodoForm from './AddTodoForm'
 
-function useSemiPersistentState() {
-  const [todoList, setTodoList] = useState(() => {
-    const savedTodoList = localStorage.getItem('savedTodoList');
-    return savedTodoList ? JSON.parse(savedTodoList) : [];
-  });
+function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true); 
 
   useEffect(() => {
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          data: {
+            todoList: JSON.parse(localStorage.getItem('savedTodoList')) || []
+          }
+        });
+      }, 2000);
+    }).then(result => {
+      setTodoList(result.data.todoList);
+      setIsLoading(false); 
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!isLoading) { 
     localStorage.setItem('savedTodoList', JSON.stringify(todoList));
-  },[todoList]);
-
-  return [todoList, setTodoList];
-
-}
-
-function App() {
-  const [todoList, setTodoList] = useSemiPersistentState();
+  }
+}, [todoList, isLoading]);
 
   const addTodo = (newTodoTitle) => {
     const newTodoItem = {
@@ -35,12 +43,17 @@ function App() {
 
   return (
     <>
-      <h1>Todo List</h1>
-      <AddTodoForm onAddTodo={addTodo}/>
-      
-      <TodoList todoList={todoList} onRemoveTodo={removeTodo}/>
+      {isLoading ? ( 
+        <p>Loading...</p> 
+      ) : ( 
+        <>
+          <h1>Todo List</h1>
+          <AddTodoForm onAddTodo={addTodo}/>
+          <TodoList todoList={todoList} onRemoveTodo={removeTodo}/>
+        </>
+      )} 
     </>
-  )
+  );
 }
 
 export default App
